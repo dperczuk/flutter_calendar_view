2450ab3ade63b7df9ac7095f3af8db7c1533832c
dperczuk <damian.perczuk@euvic.pl>
2021-12-29 19:08:42 +0100

    :lipstick: From now, title and description are centered vertically, and horizontally

--- lib/src/components/day_view_components.dart
+++ lib/src/components/day_view_components.dart
@@ -69,28 +69,32 @@ class RoundedEventTile extends StatelessWidget {
         children: [
           if (title.isNotEmpty)
             Expanded(
-              child: Text(
-                title,
-                style: titleStyle ??
-                    TextStyle(
-                      fontSize: 20,
-                      color: backgroundColor.accent,
-                    ),
-                softWrap: true,
-                overflow: TextOverflow.fade,
+              child: Center(
+                child: Text(
+                  title,
+                  style: titleStyle ??
+                      TextStyle(
+                        fontSize: 20,
+                        color: backgroundColor.accent,
+                      ),
+                  softWrap: true,
+                  overflow: TextOverflow.fade,
+                ),
               ),
             ),
           if (description.isNotEmpty)
             Expanded(
               child: Padding(
                 padding: const EdgeInsets.only(bottom: 15.0),
-                child: Text(
-                  description,
-                  style: descriptionStyle ??
-                      TextStyle(
-                        fontSize: 17,
-                        color: backgroundColor.accent.withAlpha(200),
-                      ),
+                child: Center(
+                  child: Text(
+                    description,
+                    style: descriptionStyle ??
+                        TextStyle(
+                          fontSize: 17,
+                          color: backgroundColor.accent.withAlpha(200),
+                        ),
+                  ),
                 ),
               ),
             ),
